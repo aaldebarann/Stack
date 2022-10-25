f0d0194625a3519f901b14a977c711b2c97e8233
ivanchenko.alm@gmail.com <ivanchenko.alm@gmail.com>
2022-10-25 10:11:25 +0300

    Добавьте файлы проекта.

---
+++ Main.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include "Stack.h"
+
+int main()
+{
+  Stack<int> stack;
+  cout << "New stack is empty: " << stack.empty() << endl;
+  stack.push(5);
+  stack.push(4);
+  stack.push(3);
+  cout << "Now stack is empty: " << stack.empty() << endl;
+  cout << stack.top() << endl;
+  stack.pop();
+  cout << stack.top() << endl;
+  stack.pop();
+  cout << stack.top() << endl;
+  stack.pop();
+
+  double arr[] = { 0.92, 1.1, 4.4, 6.6, 7.8 };
+  Stack<double> sd(5, arr);
+  Stack<double> sd_copy(sd);
+  while (!sd_copy.empty()) {
+    cout << sd_copy.top() << endl;
+    sd_copy.pop();
+  }
+}
